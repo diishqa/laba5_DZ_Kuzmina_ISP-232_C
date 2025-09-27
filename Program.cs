c1b5efeac9b085a6cf66cd5cd1dc991024f4e2e1
Diana <kkover073@gmail.com>
2025-09-27 22:02:59 +0300

    ne vse

--- Program.cs
+++ Program.cs
@@ -4,7 +4,145 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //задание 1
+            //Console.WriteLine("1. - Ничего");
+            //Console.WriteLine("2. -  void ShowWelcomeMessage()");
+            //Console.WriteLine("3. - Метод вызовет ошибку компиляции");
+            //Console.WriteLine("4. - void PrintNumber(int number)");
+            //Console.WriteLine("5. - PrintMessage('Hello, World!')");
+            //задание 2
+            //void GreetUser(string name)
+            //{
+            //    Console.WriteLine($"Привет, {name}! Добпро пожаловать в программу!");
+            //}
+            //Console.Write("Введите ваше имя: ");
+            //string usname = Console.ReadLine();
+            //GreetUser(usname);
+            //задание 3
+            //void AddNumbers(int n, int m)
+            //{
+            //    Console.WriteLine($"Сумма чисел: {n + m}");
+            //}
+            //Console.WriteLine("Введите 2 числа: ");
+            //int num1=Convert.ToInt32(Console.ReadLine());
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+            //AddNumbers(num1, num2 );
+            //задание 4
+            //void IsEven(int n)
+            //{
+            //    if (n % 2 == 0)
+            //    {
+            //        Console.WriteLine("true");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("false");
+            //    }
+            //}
+            //Console.Write("Введите число: ");
+            //int num=Convert.ToInt32(Console.ReadLine());
+            //IsEven(num);
+            //задание 5
+            //void FindMax(int[] chis)
+            //{
+            //    int max= 0;
+            //    foreach (int i in chis)
+            //    {
+            //        if (i > max)
+            //        {
+            //            max= i;
+            //        }
+            //    }
+            //    Console.WriteLine($"Наибольшее занчение: {max}");
+            //}
+            //int[] nums = [2, 6, 12, 86, 43, 1, 34, 6, 2, 8, 5, 3];
+            //FindMax(nums);
+            //задание 6
+            //void ConvertCelsiusToFahrenheit(int C)
+            //{
+            //    int F = C * (9 / 5) + 32;
+            //    Console.WriteLine($"{C}-->{F}");
+            //}
+            //Console.Write("Введите градусы: ");
+            //int gr = Convert.ToInt32(Console.ReadLine());
+            //ConvertCelsiusToFahrenheit(gr);
+            //задание 7
+            //void CountVowels(string str)
+            //{
+            //    int count = 0;
+            //    foreach (char c in str)
+            //    {
+            //        if (c=='a' || c=='e' || c=='i' || c=='o' || c=='u')
+            //        {
+            //            count++;
+            //        }
+            //    }
+            //    Console.WriteLine($"Кол-во гластныъ букв: {count}");
+            //}
+            //Console.WriteLine("Введите текст: ");
+            //string text = Console.ReadLine();
+            //CountVowels(text);
+            //задание 8
+            //void GeneratePassword(int lenght)
+            //{
+            //    string sym = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789";
+            //    string pas = "";
+            //    Random random = new Random();
+            //    for(int i=0; i<lenght; i++)
+            //    {
+            //        int index=random.Next(sym.Length);
+            //        pas += sym[index];
+            //    }
+            //    Console.WriteLine($"Пароль - {pas}");
+            //}
+            //Console.Write("Введите длину пароля: ");
+            //int len = Convert.ToInt32(Console.ReadLine());
+            //GeneratePassword(len);
+
+
+            //Console.ReadLine();
+            //задание 9
+            //void IsPalindrome(string str)
+            //{
+            //    int l=0;
+            //    int r=str.Length - 1;
+
+            //    while(l < r)
+            //    {
+            //        if (str[l] != str[r])
+            //        {
+            //            Console.WriteLine("не полиндром");
+            //            return;
+            //        }
+            //        l++;
+            //        r--;
+            //    }
+            //    Console.WriteLine("Полиндром");
+            //}
+            //Console.Write("Введите строку: ");
+            //string input = Console.ReadLine();
+            //IsPalindrome(input);
+            //задание 10
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+
+
         }
     }
 }
