3eb1ca3a68e5ba6ec8c47a8f6617dabf4a40f7de
Diana <kkover073@gmail.com>
2025-09-27 21:17:30 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace laba5_DZ_Kuzmina_ISP_232_C
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
